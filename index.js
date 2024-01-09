c1fb076ae350cd8095066bf4556a1f91235be001
SultanMahmud <smahmud191155@bscse.uiu.ac.bd>
2024-01-09 14:58:41 +0600

    object

---
+++ index.js
@@ -0,0 +1,16 @@
+ 
+//  let person ={
+//     name:'Mosh',
+//     age : 30
+//  };
+
+// //  dot notation
+// person.name ='john';
+
+// // Bracket Notation
+// person['name'] ='mary';
+//  console.log(person.age);
+ 
+let selectedColors =['red','green','blue'];
+selectedColors[2] = 1;
+console.log(selectedColors.length);
