2524fa8026c38b855cb9318359971ce66977108b
SultanMahmud <smahmud191155@bscse.uiu.ac.bd>
2024-01-09 15:47:16 +0600

    function

--- index.js
+++ index.js
@@ -11,6 +11,23 @@
 // person['name'] ='mary';
 //  console.log(person.age);
  
-let selectedColors =['red','green','blue'];
-selectedColors[2] = 1;
-console.log(selectedColors.length);
+// let selectedColors =['red','green','blue'];
+// selectedColors[2] = 1;
+// console.log(selectedColors.length);
+
+
+//Function
+
+function greet(name, lastname){
+    console.log('Hellow' + name +' ' + lastname)
+}
+
+greet('Sultan mahmud','Alif' );//argument supply on the peramiter
+greet('Nothing');//argument supply on the peramiter
+
+// calculating a value
+function square(number) {
+   return number*number;
+}
+
+console.log(square(2))
