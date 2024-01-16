838d1f2b064ac790bb98e479df3954acfbee74be
SultanMahmud <smahmud191155@bscse.uiu.ac.bd>
2024-01-16 14:58:33 +0600

    use-variable

--- practice-overlay-task/overlay.js
+++ practice-overlay-task/overlay.js
@@ -7,28 +7,20 @@ function startAction() {
 
   loadingProgress();
 }
-  
- 
-function loadingProgress(){
-  
 
-duration = document.getElementById("progressBar").style.animationDuration = "2s";
-
-// duration.style.animationDuration = "2s";
-
+function loadingProgress() {
+  time = 5;
+  let duration = document.getElementById("progressBar");
+  duration.style.animationDuration = time + "s";
   setTimeout(function () {
     let overlay = document.getElementById("loadingOverlay");
     overlay.style.display = "flex";
-    document.getElementById("loadingOverlay").style.display = "none";
 
     let header = document.querySelector(".header");
     header.style.backgroundColor = "black";
     header.style.color = "#fff";
-  }, parseInt(duration)*1000);
-   
+  }, time * 1000);
 }
-
-
 
 function resetAll() {
   let header = document.querySelector(".header");
